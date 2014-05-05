d604b6da68ccd4f3ba78473225dcb6acd08bfd6d
Michael Pereira <michael.pereira@cbc.ca>
2014-05-05 13:42:09 -0400

    Configging config.

--- js/config.js
+++ js/config.js
@@ -11,23 +11,23 @@ var creditLine = 'CBC News | Made with Chartbuilder';
 
 
 var defaultChartConfig = {
-    container: "#chartContainer", //css id of target chart container
-    editable: false, // reserved for enabling or dissabling on chart editing
-    lineDotsThreshold: 15, //line charts will have dots on points until a series has this number of points
-    bargridLabelMargin: 10, //the horizontal space between a bargrid bar and it's label
-    bargridBarThickness: 20, //thickness of the bars in a bargrid
-    xAxisMargin: 10, //the vertical space between the plot area and the x axis
-    footerMargin: 4, //the vertical space between the bottom of the bounding box and the meta information
-    legendLabelSpacingX: 5, //the horizontal space between legend items
-    legendLabelSpacingY: 40, //the vertical space between legend items
-    columnGap: 2, //the horizontal space between two columns that have the same x-axis value
-    axisBarGap: 5, //the horizontal space between a vertical axis and an adjacent bar
-    maxColumnWidth: 10, // the maximum width of a column as a percent of the available chart width  primaryAxisPosition: "right", // the first axis will be rendered on this side, "right" or "left" only
-    primaryAxisPosition: "right", // the first axis will be rendered on this side, "right" or "left" only
-    legend: true, // whether or not there should be a legend
-    title: "", // the chart title
-    titleBottomMargin: 5, // the vertical space between the title and the next element (sometimes a legend, sometimes an axis)
-    bargridLabelBottomMargin: 5, //the space between the bargrid series label and the top most bar
+    container: "#chartContainer",   // css id of target chart container
+    editable: false,                // reserved for enabling or dissabling on chart editing
+    lineDotsThreshold: 15,          // line charts will have dots on points until a series has this number of points
+    bargridLabelMargin: 10,         // the horizontal space between a bargrid bar and it's label
+    bargridBarThickness: 20,        // thickness of the bars in a bargrid
+    xAxisMargin: 10,                // the vertical space between the plot area and the x axis
+    footerMargin: 4,                // the vertical space between the bottom of the bounding box and the meta information
+    legendLabelSpacingX: 5,         // the horizontal space between legend items
+    legendLabelSpacingY: 40,        // the vertical space between legend items
+    columnGap: 2,                   // the horizontal space between two columns that have the same x-axis value
+    axisBarGap: 5,                  // the horizontal space between a vertical axis and an adjacent bar
+    maxColumnWidth: 10,             // the maximum width of a column as a percent of the available chart width  primaryAxisPosition: "right", // the first axis will be rendered on this side, "right" or "left" only
+    primaryAxisPosition: "right",   // the first axis will be rendered on this side, "right" or "left" only
+    legend: true,                   // whether or not there should be a legend
+    title: "",                      // the chart title
+    titleBottomMargin: 5,           // the vertical space between the title and the next element (sometimes a legend, sometimes an axis)
+    bargridLabelBottomMargin: 5,    // the space between the bargrid series label and the top most bar
     colors: [
                 '#125278', '#417593', '#7197AE', '#A0BAC9', '#D0DCE4',
                 '#1E91B4', '#4BA7C3', '#78BDD2', '#A5D3E1', '#D2E9F0',
